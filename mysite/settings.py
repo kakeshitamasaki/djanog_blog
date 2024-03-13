a6abc8be4ef158b1d0cd016831f71f02c22baa64
kake <kakeshitamasaki@gmail.com>
2024-03-13 23:04:54 +0900

    aa

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ky)j9)95=j&w%fx2@r32#00)s)kmtaip%)ln@j&_c$lt!*i%4v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
